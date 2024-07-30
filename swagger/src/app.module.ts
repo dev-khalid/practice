33905a783b704ab7c6e0d7256084d5be439fc827
khalid-fn <khalid.hossain@fieldnation.com>
2024-07-30 12:14:42 +0600

    finding issues

--- swagger/src/app.module.ts
+++ swagger/src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { body, bodyFiltered, model } from './object.response';
+console.log(!!bodyFiltered)
 @Module({
   imports: [],
   controllers: [AppController],
---
+++ swagger/terminal-report-of-unexpected-properties-analysis.ps1
@@ -0,0 +1,144 @@
+{ objValue: undefined, srcValue: 0, key: 'sites' }
+{ objValue: undefined, srcValue: 0, key: 'payment' }
+{ objValue: undefined, srcValue: 0, key: 'client_payment_enabled' }
+{ objValue: undefined, srcValue: 0, key: 'client_payment_terms' }
+{ objValue: undefined, srcValue: 0, key: 'can_view' }
+{ objValue: undefined, srcValue: 0, key: 'buyer_approval_time_days' }
+{ objValue: undefined, srcValue: 0, key: 'client_payment_terms_copy' }
+{
+  objValue: undefined,
+  srcValue: 0,
+  key: 'client_payment_terms_updated_copy'
+}
+{ objValue: undefined, srcValue: 0, key: 'target_review_period' }
+{ objValue: undefined, srcValue: 0, key: 'actions' }
+{ objValue: undefined, srcValue: 0, key: 'correlation_id' }
+{ objValue: undefined, srcValue: 0, key: 'pricing_insights' }
+{ objValue: undefined, srcValue: 0, key: 'positive' }
+{ objValue: undefined, srcValue: 0, key: 'currency_style' }
+{ objValue: undefined, srcValue: 0, key: 'fn_process_payment' }
+{ objValue: undefined, srcValue: 0, key: 'total_by_role' }
+{ objValue: undefined, srcValue: 0, key: 'total_before_fees_by_role' }
+{ objValue: undefined, srcValue: 0, key: 'is_taxable_location' }
+{ objValue: undefined, srcValue: 0, key: 'pay_old' }
+{
+  objValue: undefined,
+  srcValue: {
+    workOrderId: 0,
+    userId: 0,
+    lineItems: 0,
+    fees: 0,
+    totals: 0,
+    ledger: 0,
+    attributes: 0,
+    from_cache: 0
+  },
+  key: 'pay_service'
+}
+{ objValue: undefined, srcValue: 0, key: 'workOrderId' }
+{ objValue: undefined, srcValue: 0, key: 'userId' }
+{ objValue: undefined, srcValue: 0, key: 'lineItems' }
+{ objValue: undefined, srcValue: 0, key: 'fees' }
+{ objValue: undefined, srcValue: 0, key: 'totals' }
+{ objValue: undefined, srcValue: 0, key: 'ledger' }
+{ objValue: undefined, srcValue: 0, key: 'attributes' }
+{ objValue: undefined, srcValue: 0, key: 'from_cache' }
+{ objValue: undefined, srcValue: 0, key: 'has_preferred_requests' }
+{ objValue: undefined, srcValue: 0, key: 'has_preferred_counters' }
+{ objValue: undefined, srcValue: 0, key: 'counter_offers' }
+{ objValue: undefined, srcValue: 0, key: 'routes' }
+{ objValue: undefined, srcValue: 0, key: 'declines' }
+{ objValue: undefined, srcValue: 0, key: 'all_requests' }
+{ objValue: undefined, srcValue: 0, key: 'correlation_id' }
+{ objValue: undefined, srcValue: 0, key: 'requests' }
+{
+  objValue: undefined,
+  srcValue: { correlation_id: 0, buyer: 0, assigned_provider: 0 },
+  key: 'ratings'
+}
+{ objValue: undefined, srcValue: 0, key: 'correlation_id' }
+{ objValue: undefined, srcValue: 0, key: 'buyer' }
+{ objValue: undefined, srcValue: 0, key: 'assigned_provider' }
+{ objValue: undefined, srcValue: 0, key: 'actions' }
+{ objValue: undefined, srcValue: 0, key: 'conflicts' }
+{ objValue: undefined, srcValue: 0, key: 'actions' }
+{ objValue: undefined, srcValue: 0, key: 'actions' }
+{ objValue: undefined, srcValue: 0, key: 'holds' }
+{ objValue: undefined, srcValue: { id: 1, name: 1 }, key: 'project' }
+{ objValue: undefined, srcValue: 1, key: 'id' }
+{ objValue: undefined, srcValue: 1, key: 'name' }
+{ objValue: undefined, srcValue: 1, key: 'email' }
+{ objValue: undefined, srcValue: 0, key: 'actions' }
+{ objValue: undefined, srcValue: 0, key: 'time_to_dispatch' }
+{ objValue: undefined, srcValue: 0, key: 'time_to_work_done' }
+{ objValue: undefined, srcValue: 0, key: 'time_alive' }
+{ objValue: undefined, srcValue: 0, key: 'time_to_approve' }
+{
+  objValue: undefined,
+  srcValue: { id: 0, name: 0 },
+  key: 'service_contract'
+}
+{ objValue: undefined, srcValue: 0, key: 'id' }
+{ objValue: undefined, srcValue: 0, key: 'name' }
+{ objValue: undefined, srcValue: { id: 1, name: 1 }, key: 'template' }
+{ objValue: undefined, srcValue: 1, key: 'id' }
+{ objValue: undefined, srcValue: 1, key: 'name' }
+{ objValue: undefined, srcValue: { id: 0, name: 0 }, key: 'team' }
+{ objValue: undefined, srcValue: 0, key: 'id' }
+{ objValue: undefined, srcValue: 0, key: 'name' }
+{ objValue: undefined, srcValue: 1, key: 'integration' }
+{ objValue: null, srcValue: 'object', key: 'integrations' }
+{ objValue: undefined, srcValue: 0, key: 'high_potential' }
+{
+  objValue: undefined,
+  srcValue: {
+    id: 0,
+    required: 0,
+    certification_id: 0,
+    dynamic_term_id: 0,
+    name: 0,
+    feeReduction: 0,
+    fee: 0,
+    international: 0
+  },
+  key: 'special_skill'
+}
+{ objValue: undefined, srcValue: 0, key: 'id' }
+{ objValue: undefined, srcValue: 0, key: 'required' }
+{ objValue: undefined, srcValue: 0, key: 'certification_id' }
+{ objValue: undefined, srcValue: 0, key: 'dynamic_term_id' }
+{ objValue: undefined, srcValue: 0, key: 'name' }
+{ objValue: undefined, srcValue: 0, key: 'feeReduction' }
+{ objValue: undefined, srcValue: 0, key: 'fee' }
+{ objValue: undefined, srcValue: 0, key: 'international' }
+{ objValue: undefined, srcValue: 0, key: 'actions' }
+{ objValue: undefined, srcValue: 0, key: 'pending_routing_provider' }
+{
+  objValue: undefined,
+  srcValue: {
+    fn_one_overage: 0,
+    marketplace_overage: 0,
+    general_liability_liaison: 0,
+    applied_constant_hourly_rate: 0,
+    survey_id: 0
+  },
+  key: 'attributes'
+}
+{ objValue: undefined, srcValue: 0, key: 'fn_one_overage' }
+{ objValue: undefined, srcValue: 0, key: 'marketplace_overage' }
+{ objValue: undefined, srcValue: 0, key: 'general_liability_liaison' }
+{
+  objValue: undefined,
+  srcValue: 0,
+  key: 'applied_constant_hourly_rate'
+}
+{ objValue: undefined, srcValue: 0, key: 'survey_id' }
+{ objValue: undefined, srcValue: 1, key: 'utc_convert_off' }
+{ objValue: undefined, srcValue: 0, key: 'role' }
+{ objValue: undefined, srcValue: 0, key: 'workflow_completion' }
+{ objValue: undefined, srcValue: 0, key: 'actions' }
+{ objValue: undefined, srcValue: 0, key: 'correlation_id' }
+{ objValue: undefined, srcValue: 0, key: 'workflow_publish_or_route' }
+{ objValue: undefined, srcValue: 0, key: 'workflow_cancel' }
+{ objValue: undefined, srcValue: 0, key: 'timings' }
+{ objValue: undefined, srcValue: 1, key: 'auto_dispatch' }
