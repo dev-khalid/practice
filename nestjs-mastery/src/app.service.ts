2f9674d2594cee9a62dfc7d2273be078c4ed6ffc
dev-khalid <khalid.hossain.akash@gmail.com>
2024-08-26 09:49:45 +0600

    method decorator

--- nestjs-mastery/src/app.service.ts
+++ nestjs-mastery/src/app.service.ts
@@ -11,21 +11,49 @@ function RequestValidator() {
         let log = args?.[0]?.params?.join(' ');
         //run and store the result!
         let result = originalMethod.apply(this, args);
-        console.log('what is the mening of this?', result);
         return result;
       }
     };
     return descriptor;
   };
 }
+function deprecatedMethod() {
+  return function (target: any, key: string, descriptor: PropertyDescriptor) {
+    let originalMethod = descriptor.value;
+    descriptor.value = function (...args: any[]) {
+      console.log(
+        `${key} is deprecated and will be removed in a future release!`
+      );
+      return originalMethod.apply(this, args);
+    };
+  };
+}
+
+function optionalDelay(seconds: number) {
+  return function (target: any, key: string, descriptor: PropertyDescriptor) {
+    let originalMethod = descriptor.value;
+    descriptor.value = async function (...args: any[]) {
+      console.log(`Delaying ${seconds} seconds before executing ${key}`);
+      await new Promise((resolve) => setTimeout(resolve, seconds * 1000));
+      return originalMethod.apply(this, args);
+    };
+  };
+}
+
+
+
 @Injectable()
 export class AppService {
+  private appServiceVersion: string = 'v1';
   @RequestValidator()
   getHello(input?: { params?: string[] }): string {
     return input?.params?.join(' ') ?? 'Hello World!';
   }
 
+  @deprecatedMethod()
+  @optionalDelay(5)
   healthCheck(res: Response) {
     return res.json(this.getHello({ params: ['Hello', 'Khalid'] }));
+    // return res.json({ status: 'UP' });
   }
 }
--- typescript-mastery/src/index.ts
+++ typescript-mastery/src/index.ts
@@ -1,3 +1,7 @@
+/**
+ * Useful links:
+ * - https://blog.logrocket.com/practical-guide-typescript-decorators/#class-decorators
+ */
 function withFuel(target: typeof Rocket, context: ClassDecoratorContext) {
   if (context.kind == "class") {
     return class extends target {
@@ -11,23 +15,58 @@ function withFuel(target: typeof Rocket, context: ClassDecoratorContext) {
 function logFuel(target: Function, context: ClassDecoratorContext) {
   const originalMethod = target.prototype.addFuel;
   target.prototype.addFuel = function (...args: any[]) {
-    console.log(`Before adding fuel, total fuel: ${this.fuel}`); 
-    originalMethod.call(this, args?.[0]); 
+    console.log(`Before adding fuel, total fuel: ${this.fuel}`);
+    originalMethod.call(this, args?.[0]);
     console.log(`After adding fuel, total fuel: ${this.fuel}`);
   };
 }
 
+function deprecatedMethod(
+  target: Function,
+  context: ClassMethodDecoratorContext
+) {
+  if (context.kind == "method") {
+    console.warn(
+      `${String(
+        context.name
+      )} is deprecated and will be removed in a future release!`
+    );
+    // return target.apply(this, arguments);
+  }
+}
+function deprecatedProperty(_: any, context: ClassFieldDecoratorContext) {
+  if (context.kind == "field") {
+    return function (initialValue: any) {
+      console.warn(
+        `Property ${String(
+          context.name
+        )} is deprecated and will be removed in a future release!`
+      );
+      return initialValue;
+    };
+  }
+}
 @withFuel
 @logFuel
 class Rocket {
   public fuel: number = 0;
+
+  @deprecatedProperty
+  public rocketName: string;
+
+  constructor() {}
   addFuel(amount: number) {
     this.fuel += amount;
   }
-   
+
+  // @deprecatedMethod
+  isReadyForLaunch() {
+    return this.fuel > 0;
+  }
 }
 
-const rocket = new Rocket();
+// const rocket = new Rocket();
 
-rocket.addFuel(50);
-console.log(rocket.fuel);
+// rocket.addFuel(50);
+// // console.log(rocket.fuel);
+// console.log(rocket.isReadyForLaunch());
